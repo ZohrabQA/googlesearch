b0679295694861fefd72ff3f953244668814b7a3
Zohrab <130496323+ZohrabQA@users.noreply.github.com>
2023-05-20 15:12:56 +0400

    Update googlesearch.py

--- googlesearch.py
+++ googlesearch.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.keys import Keys
 path = "/Users/zuzu/Downloads/chromedriver_mac64/chromedriver"
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(path,options=options)
+driver = webdriver.Chrome(path, options=options)
 driver.maximize_window()
 driver.get("https://www.google.com/")
-serach = driver.find.element(By.XPATH, '//*[@id="APjFqb"]')
-serach.send.keys("Transformers", Keys.ENTER)
+axtar = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
+axtar.send_keys("Transformers",Keys.ENTER)
